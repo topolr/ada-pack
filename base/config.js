17063b7d757251e650cb1888930750ee9e390c97
Jinliang <hou80houzhu@vip.qq.com>
2017-12-02 00:14:27 +0800

    pass

--- base/config.js
+++ base/config.js
@@ -15,12 +15,12 @@ module.exports = {
     keywords:["ada"],
     pages: [],
     icons: [
-        {"src": "images/48@2x.png", "sizes": "48x48", "type": "image/png"},
-        {"src": "images/72@2x.png", "sizes": "72x72", "type": "image/png"},
-        {"src": "images/96@2x.png", "sizes": "96x96", "type": "image/png"},
-        {"src": "images/144@2x.png", "sizes": "144x144", "type": "image/png"},
-        {"src": "images/168@2x.png", "sizes": "168x168", "type": "image/png"},
-        {"src": "images/192@2x.png", "sizes": "192x192", "type": "image/png"}
+        {"src": "icons/48@2x.png", "sizes": "48x48", "type": "image/png"},
+        {"src": "icons/72@2x.png", "sizes": "72x72", "type": "image/png"},
+        {"src": "icons/96@2x.png", "sizes": "96x96", "type": "image/png"},
+        {"src": "icons/144@2x.png", "sizes": "144x144", "type": "image/png"},
+        {"src": "icons/168@2x.png", "sizes": "168x168", "type": "image/png"},
+        {"src": "icons/192@2x.png", "sizes": "192x192", "type": "image/png"}
     ],
     related_applications: [{"platform": "web"}],
     page: {
