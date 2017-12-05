4683bcc6fe5a1cc4c367f163a1bb95710c978de9
hou80houzhu <hou80houzhu@vip.qq.com>
2017-12-05 16:23:28 +0800

    pass

--- dev/index.js
+++ dev/index.js
@@ -8,20 +8,22 @@ function runDev() {
     let express = require(Path.resolve(projectPath, "./node_modules/express"));
     let packagePath = Path.resolve(projectPath, "./package.json");
     let package = JSON.parse(new File(packagePath).readSync());
-    if (package.adaDev) {
-        let port = package.adaDev.port;
-        let appPath = Path.resolve(packagePath, "./../", package.adaDev.appPath);
-        let appInfo = util.getAppInfo(appPath);
-        let distPath = Path.resolve(appPath, "./../", appInfo.dist_path);
-        let app = new express();
-        app.use(express.static(distPath));
-        app.get("/", (req, res) => {
-            res.send(require("fs").readFileSync(Path.resolve(distPath, "./index.html"), "utf-8"));
-        });
-        app = adaSSE(app);
-        app.listenDev(appPath, port);
-    } else {
-        throw Error("package can not contain adaDev set");
+    if (!package.adaDev) {
+        package.adaDev = {
+            port: 8080,
+            appPath: "./app/app.js"
+        };
     }
+    let port = package.adaDev.port;
+    let appPath = Path.resolve(packagePath, "./../", package.adaDev.appPath);
+    let appInfo = util.getAppInfo(appPath);
+    let distPath = Path.resolve(appPath, "./../", appInfo.dist_path);
+    let app = new express();
+    app.use(express.static(distPath));
+    app.get("/", (req, res) => {
+        res.send(require("fs").readFileSync(Path.resolve(distPath, "./index.html"), "utf-8"));
+    });
+    app = adaSSE(app);
+    app.listenDev(appPath, port);
 };
 runDev();
