85a2c2ff1d695d4f2844856c47eadc0cf7405c7e
hou80houzhu <hou80houzhu@vip.qq.com>
2017-12-07 17:22:47 +0800

    pass

--- base/maker/htmlmaker.js
+++ base/maker/htmlmaker.js
@@ -10,19 +10,21 @@ module.exports = function (content, path, option) {
             content = util.replacePaths(content, (_path) => {
                 let r = "";
                 let o = Path.resolve(path, "./../", _path).replace(/\\/g, "/");
-                if (o.indexOf("node_modules") === -1) {
-                    r = Path.resolve(path, "./../", _path).substring(option.source_path.length).replace(/\\/g, "/");
-                } else {
-                    r = "node_modules/" + Path.resolve(path, "./../", _path).substring(option.nmodule_path.length).replace(/\\/g, "/");
-                }
-                let d = Path.resolve(option.dist_path, r).replace(/\\/g, "/");
-                if (option.develop) {
-                    new File(o).copyTo(d);
-                    return option.site_url + r;
-                } else {
-                    let hash = new File(o).hash().substring(0, 8);
-                    new File(o).copyTo(util.getHashPath(d, hash));
-                    return option.site_url + util.getHashPath(r, hash);
+                if(o.indexOf("{{")===-1) {
+                    if (o.indexOf("node_modules") === -1) {
+                        r = Path.resolve(path, "./../", _path).substring(option.source_path.length).replace(/\\/g, "/");
+                    } else {
+                        r = "node_modules/" + Path.resolve(path, "./../", _path).substring(option.nmodule_path.length).replace(/\\/g, "/");
+                    }
+                    let d = Path.resolve(option.dist_path, r).replace(/\\/g, "/");
+                    if (option.develop) {
+                        new File(o).copyTo(d);
+                        return option.site_url + r;
+                    } else {
+                        let hash = new File(o).hash().substring(0, 8);
+                        new File(o).copyTo(util.getHashPath(d, hash));
+                        return option.site_url + util.getHashPath(r, hash);
+                    }
                 }
             });
             // resolve(minify(content, Object.assign({
@@ -33,7 +35,7 @@ module.exports = function (content, path, option) {
             // }, option.minifier)));
             resolve(content);
         } catch (e) {
-            reject(content);
+            reject(e);
         }
     });
 };
