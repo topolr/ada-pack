0f36d54447a8b7556f9510bb2545e0fae1b7639c
hou80houzhu <hou80houzhu@vip.qq.com>
2017-12-08 10:32:27 +0800

    pass

--- base/bundler.js
+++ base/bundler.js
@@ -8,6 +8,8 @@ let queue = require("./lib/queue");
 let isbinaryfile = require("isbinaryfile");
 let config = require("./config");
 let gzipSize = require('gzip-size');
+let less = require("less");
+let minify = require('html-minifier').minify;
 
 const THRIDPARTFOLDER = "node_modules";
 const IGNOREMODULES = ["fs", "path", "util", "http", "events", "crypto", "adajs"];
@@ -17,6 +19,44 @@ class AdaBundler {
     constructor() {
         this.resultmap = [];
         this.resultmapcode = {};
+    }
+
+    getFileCode(path) {
+        return new Promise((resolve, reject) => {
+            let file = new File(path), suffix = file.suffix();
+            if (suffix === "html") {
+                resolve(`module.exports=${JSON.stringify(file.readSync().replace(/\n/g, '').replace(/\r/g, '').replace(/\n\r/g, ''))}`);
+            } else if (suffix === "less") {
+                let code = file.readSync();
+                less.render(content, function (e, output) {
+                    if (!e) {
+                        code = output.css;
+                        resolve(`var _a = document.createElement("style");_a.setAttribute("media", "screen");_a.setAttribute("type", "text/css");_a.appendChild(document.createTextNode(${code}));document.body.appendChild(_a);module.exports=_a;`);
+                    }
+                });
+            } else if (suffix === "icon") {
+                let content = file.readSync();
+                content = minify(content, {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    minifyJS: true,
+                    minifyCSS: true
+                });
+                let titleTag = content.match(/<title>[\s\S]*?>/);
+                let name = "";
+                if (titleTag) {
+                    name = titleTag[0].substring(7, titleTag[0].length - 8).trim();
+                }
+                let et = content.replace(/svg/g, "symbol").replace(/xmlns\=".*?"/, "").replace(/version\=".*?"/, "").replace(/viewBox\=".*?"/, (str) => {
+                    return `${str} id="${name}"`;
+                });
+                let code = `<svg style="width:0;height:0;overflow:hidden;" version="1.1" xmlns="http://www.w3.org/2000/svg">${et}</svg>`;
+                let result = `var c=document.getElementById("ada-icon-container");if(!c){var c=document.createElement("div");c.setAttribute("id","ada-icon-container");c.style.cssText="width:0;height:0;";document.body.appendChild(c);}if(!document.getElementById("${name}")){var a=document.createElement("div");a.innerHTML=${JSON.stringify(code)};c.appendChild(a.childNodes[0]);}module.exports="${name}"`;
+                resolve(result);
+            } else {
+                resolve(util.babelCode(config, file.readSync()));
+            }
+        });
     }
 
     getDependenceInfo(path, code) {
@@ -48,8 +88,10 @@ class AdaBundler {
     }
 
     getCodeMap(path) {
-        this.getDependenceInfo(path, util.babelCode(config, new File(path).readSync())).forEach(path => {
-            this.getCodeMap(path);
+        this.getFileCode(path).then(code => {
+            this.getDependenceInfo(path, code).forEach(path => {
+                this.getCodeMap(path);
+            });
         });
     }
 
