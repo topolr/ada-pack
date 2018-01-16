cfe82e256f530742c4b9e81f6e0403c47ca981ff
hou80houzhu <hou80houzhu@vip.qq.com>
2018-01-16 17:58:09 +0800

    pass

--- base/maker/jsmaker.js
+++ base/maker/jsmaker.js
@@ -1,22 +1,19 @@
 let babel = require("@babel/core");
 let uglify = require("uglify-js");
-let pollyfill = require("./../pollyfills/polyfill");
+let classPropertiesPollyfill = require("./../pollyfills/class-properties");
 module.exports = function (content, path, option) {
     return new Promise((resolve, reject) => {
         try {
             content = babel.transform(content, option.compiler.babel).code;
-            pollyfill.pollyfill("js", path, content, option).then(content => {
-                try {
-                    return uglify.minify(content, Object.assign({
-                        fromString: true,
-                        mangle: true
-                    }, option.compiler.uglify)).code;
-                } catch (e) {
-                    return content;
-                }
-            }).then(content => {
-                resolve(content);
-            });
+            content = classPropertiesPollyfill(content);
+            try {
+                content = uglify.minify(content, Object.assign({
+                    fromString: true,
+                    mangle: true
+                }, option.compiler.uglify)).code;
+            } catch (e) {
+            }
+            resolve(content);
         } catch (e) {
             reject(e);
         }
--- base/pollyfills/class-properties.js
+++ base/pollyfills/class-properties.js
@@ -1,6 +1,5 @@
-module.exports = function (content, path, option) {
-    let result = content.replace(/_initializerDefineProperty[\s\S]+?\}/g, function (str) {
+module.exports = function (content) {
+    return content.replace(/_initializerDefineProperty[\s\S]+?\}/g, function (str) {
         return str.replace(/descriptor.writable/g, "true");
     });
-    return Promise.resolve(result);
 };
