fb2dc1d03a4bb4e952d81f7cc4077c33005b8221
Jinliang <hou80houzhu@vip.qq.com>
2018-01-18 23:04:26 +0800

    pass

--- base/maker/tsmaker.js
+++ base/maker/tsmaker.js
@@ -4,12 +4,12 @@ module.exports = function (content, path, config) {
     return new Promise((resolve, reject) => {
         let file = path.substring(config.source_path.length);
         let args = ["--target ES5", "--noEmit", "--pretty", "--skipLibCheck", "--experimentalDecorators", "--version"];
-        require("child_process").exec(`node ${Path.resolve(config.base_path, "./../node_modules/typescript")}/bin/tsc ${file} ${args.join(" ")}`, {
+        require("child_process").exec(`node ${Path.resolve(config.base_path, "./node_modules/typescript")}/bin/tsc ${file} ${args.join(" ")}`, {
             encoding: "utf-8",
             cwd: config.source_path
         }, (error, stdout, stderr) => {
             if (error) {
-                error.message = stdout;
+                error.message = stdout||stderr;
                 reject(error);
             } else {
                 jsmaker(content, path, config).then((content) => {
