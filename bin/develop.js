a0f1397871ffd749f4d12b4c922b911675f7b47e
Jinliang <hou80houzhu@vip.qq.com>
2018-01-24 23:35:47 +0800

    pass

--- bin/develop.js
+++ bin/develop.js
@@ -44,62 +44,63 @@ function runDev() {
     if (!new File(appPath).isExists()) {
         appPath = Path.resolve(projectPath, "./app.js");
     }
-    let appInfo = util.getAppInfo(appPath);
-    let distPath = Path.resolve(appPath, "./../", appInfo.dist_path);
-    let serverPath = Path.resolve(projectPath, packageInfo["ada-develop"].serverPath);
-    let app = null;
-    if (!new File(serverPath).isExists()) {
-        app = new express();
-    } else {
-        app = require(serverPath);
-    }
-    app.use(express.static(distPath));
-    app.get("/", (req, res) => {
-        res.send(require("fs").readFileSync(Path.resolve(distPath, "./index.html"), "utf-8"));
-    });
-    app.use("/ada/sse", (req, res) => {
-        connected = true;
-        res.writeHead(200, {
-            'Connection': 'keep-alive',
-            'Content-Type': 'text/event-stream',
-            'Cache-Control': 'no-cache'
+    util.getAppInfo(appPath).then(appInfo => {
+        let distPath = Path.resolve(appPath, "./../", appInfo.dist_path);
+        let serverPath = Path.resolve(projectPath, packageInfo["ada-develop"].serverPath);
+        let app = null;
+        if (!new File(serverPath).isExists()) {
+            app = new express();
+        } else {
+            app = require(serverPath);
+        }
+        app.use(express.static(distPath));
+        app.get("/", (req, res) => {
+            res.send(require("fs").readFileSync(Path.resolve(distPath, "./index.html"), "utf-8"));
         });
-        res.write(`retry: ${waitTime}\n`);
-        messageQueue.subscribe((info) => {
-            res.write("id: " + Date.now() + "\ndata: " + JSON.stringify(info) + "\n\n");
+        app.use("/ada/sse", (req, res) => {
+            connected = true;
+            res.writeHead(200, {
+                'Connection': 'keep-alive',
+                'Content-Type': 'text/event-stream',
+                'Cache-Control': 'no-cache'
+            });
+            res.write(`retry: ${waitTime}\n`);
+            messageQueue.subscribe((info) => {
+                res.write("id: " + Date.now() + "\ndata: " + JSON.stringify(info) + "\n\n");
+            });
         });
-    });
-    require("./../index").develop(appPath, ({type, files, map, log}) => {
-        messageQueue.add({type, files, map, log});
-    }).then(() => {
-        app.listen(port, () => {
-            console.log("");
-            console.log(` ▶ SERVER RUNNING LOCALHOST PORT [: ${port}] `.yellow);
-            let desc = `    now try to open the page...`;
-            process.stderr.write(desc.grey);
-            process.stderr.cursorTo(desc.length);
-            let count = waitTime / 1000;
-            let num = 0;
-            let intevalId = setInterval(() => {
-                num += 1;
-                process.stderr.clearLine();
-                process.stderr.cursorTo(0);
-                if (connected) {
-                    clearInterval(intevalId);
-                    messageQueue.add({type: "reload"});
-                    process.stderr.write(`    page is opened,reload it`.grey);
-                    process.stderr.write(`\n`);
-                } else if (num === count) {
-                    clearInterval(intevalId);
-                    opn(`http://${host}:${port}`);
-                    process.stderr.write(`    can not found opened page,open it`.grey);
-                    process.stderr.write(`\n`);
-                } else {
-                    let rdesc = `    now check [${num}] times ...`;
-                    process.stderr.write(rdesc.grey);
-                    process.stderr.cursorTo(rdesc.length);
-                }
-            }, 1000);
+        require("./../index").develop(appPath, ({type, files, map, log}) => {
+            messageQueue.add({type, files, map, log});
+        }).then(() => {
+            app.listen(port, () => {
+                console.log("");
+                console.log(` ▶ SERVER RUNNING LOCALHOST PORT [: ${port}] `.yellow);
+                let desc = `    now try to open the page...`;
+                process.stderr.write(desc.grey);
+                process.stderr.cursorTo(desc.length);
+                let count = waitTime / 1000;
+                let num = 0;
+                let intevalId = setInterval(() => {
+                    num += 1;
+                    process.stderr.clearLine();
+                    process.stderr.cursorTo(0);
+                    if (connected) {
+                        clearInterval(intevalId);
+                        messageQueue.add({type: "reload"});
+                        process.stderr.write(`    page is opened,reload it`.grey);
+                        process.stderr.write(`\n`);
+                    } else if (num === count) {
+                        clearInterval(intevalId);
+                        opn(`http://${host}:${port}`);
+                        process.stderr.write(`    can not found opened page,open it`.grey);
+                        process.stderr.write(`\n`);
+                    } else {
+                        let rdesc = `    now check [${num}] times ...`;
+                        process.stderr.write(rdesc.grey);
+                        process.stderr.cursorTo(rdesc.length);
+                    }
+                }, 1000);
+            });
         });
     });
 }
