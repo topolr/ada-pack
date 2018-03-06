3c4c1be6eba9c1d2586772fcf94d371f30b97613
hou80houzhu <hou80houzhu@vip.qq.com>
2018-03-06 18:48:48 +0800

    pass

--- base/maker/iconmaker.js
+++ base/maker/iconmaker.js
@@ -17,7 +17,7 @@ module.exports = function (content, option) {
                 return `${str} id="${name}"`;
             });
             let code = `<svg style="width:0;height:0;overflow:hidden;" version="1.1" xmlns="http://www.w3.org/2000/svg">${et}</svg>`;
-            let result = `function active(){var c=document.getElementById("ada-icon-container");if(!c){var c=document.createElement("div");c.setAttribute("id","ada-icon-container");c.style.cssText="width:0;height:0;";document.body.appendChild(c);}if(!document.getElementById("${name}")){var a=document.createElement("div");a.innerHTML=${JSON.stringify(code)};c.appendChild(a.childNodes[0]);}}if (/complete|loaded|interactive/.test(window.document.readyState)) {active();} else {window.document.addEventListener('DOMContentLoaded', function () {active();}, false);}module.exports="${name}";`;
+            let result = `function active(){var c=document.getElementById("ada-icon-container");if(!c){var c=document.createElement("div");c.setAttribute("id","ada-icon-container");c.style.cssText="width:0;height:0;";document.body.appendChild(c);}if(!document.getElementById("icon-${name}")){var a=document.createElement("div");a.setAttribute("id","icon-${name}");a.innerHTML=${JSON.stringify(code)};c.appendChild(a.childNodes[0]);}}if (/complete|loaded|interactive/.test(window.document.readyState)) {active();} else {window.document.addEventListener('DOMContentLoaded', function () {active();}, false);}module.exports="${name}";`;
             resolve(result);
         } catch (e) {
             reject(e);
