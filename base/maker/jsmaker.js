8d9d3f3dc8c8d588b39ec7dcfb0f389a10d0d710
wangjinliang <wangjinliang@huobi.com>
2018-05-31 16:06:37 +0800

    pass

--- base/maker/jsmaker.js
+++ base/maker/jsmaker.js
@@ -3,26 +3,28 @@ let UglifyJS = require("uglify-es");
 let classPropertiesPollyfill = require("./../pollyfills/class-properties");
 
 module.exports = function (content, path, option) {
-    return new Promise((resolve, reject) => {
-        try {
-            if (option.develop) {
-                let info = babel.transform(content, Object.assign({
-                    filename: path.indexOf("node_modules") === -1 ? path.substring(option.source_path.length) : "node_modules/" + path.substring(option.nmodule_path.length),
-                    sourceMaps: "both"
-                }, option.compiler.babel));
-                content = info.code;
-                content = classPropertiesPollyfill(content);
-            } else {
-                content = babel.transform(content, option.compiler.babel).code;
-                content = classPropertiesPollyfill(content);
-                try {
-                    content = UglifyJS.minify(content, Object.assign({}, option.compiler.uglify)).code;
-                } catch (e) {
-                }
-            }
-            resolve(content);
-        } catch (e) {
-            reject(e);
-        }
-    });
+	return new Promise((resolve, reject) => {
+		try {
+			if (option.develop) {
+				let info = babel.transform(content, Object.assign({
+					filename: path.indexOf("node_modules") === -1 ? path.substring(option.source_path.length) : "node_modules/" + path.substring(option.nmodule_path.length),
+					sourceMaps: "both"
+				}, option.compiler.babel));
+				content = info.code;
+				content = classPropertiesPollyfill(content);
+			} else {
+				let ops=Object.assign({},option.compiler.babel);
+				ops.filename=path;
+				content = babel.transform(content, ops).code;
+				content = classPropertiesPollyfill(content);
+				try {
+					content = UglifyJS.minify(content, Object.assign({}, option.compiler.uglify)).code;
+				} catch (e) {
+				}
+			}
+			resolve(content);
+		} catch (e) {
+			reject(e);
+		}
+	});
 };
