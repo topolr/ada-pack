08b5aa03a5be2e3ca0603ca6cb438a299e2a4e18
wangjinliang <wangjinliang@huobi.com>
2018-11-06 20:32:49 +0800

    + cli support

--- bin/cmd/develop.js
+++ bin/cmd/develop.js
@@ -26,40 +26,41 @@ module.exports = {
 		let waitTime = 5000;
 		let appInfo = helper.getAppInfo("/Users/jinliang/git/ada");
 		let port = appInfo.server.port;
-		new DevServer(appInfo).start().then(app => {
-			app.use("/ada/sse", (req, res) => {
-				res.writeHead(200, {
-					'Connection': 'keep-alive',
-					'Content-Type': 'text/event-stream',
-					'Cache-Control': 'no-cache'
-				});
-				res.write(`retry: ${waitTime}\n`);
-				res.write("id: " + Date.now() + "\ndata:{}\n\n");
-				messageQueue.subscribe((info) => {
-					res.write("id: " + Date.now() + "\ndata: " + JSON.stringify(info) + "\n\n");
-				});
-			});
-			return require("../../index").develop(appInfo, ({type, files, map, log}) => {
-				messageQueue.add({type, files, map, log});
-			});
+		return require("../../index").develop(appInfo, ({type, files, map, log}) => {
+			messageQueue.add({type, files, map, log});
 		}).then(() => {
-			let spinner = ora({
-				color: "yellow",
-				text: `OPEN OR REFRESH BROWSER`
-			}).start();
-			let count = waitTime / 1000, num = 0;
-			let intevalId = setInterval(() => {
-				num += 1;
-				if (connected) {
-					clearInterval(intevalId);
-					messageQueue.add({type: "reload"});
-					spinner.stop();
-				} else if (num === count) {
-					clearInterval(intevalId);
-					opn(`http://localhost:${port}`);
-					spinner.stop();
-				}
-			}, 1000);
+			new DevServer(appInfo).start().then(app => {
+				app.use("/ada/sse", (req, res) => {
+					res.writeHead(200, {
+						'Connection': 'keep-alive',
+						'Content-Type': 'text/event-stream',
+						'Cache-Control': 'no-cache'
+					});
+					res.write(`retry: ${waitTime}\n`);
+					res.write("id: " + Date.now() + "\ndata:{}\n\n");
+					messageQueue.subscribe((info) => {
+						res.write("id: " + Date.now() + "\ndata: " + JSON.stringify(info) + "\n\n");
+					});
+				});
+			}).then(() => {
+				let spinner = ora({
+					color: "yellow",
+					text: `OPEN OR REFRESH BROWSER`
+				}).start();
+				let count = waitTime / 1000, num = 0;
+				let intevalId = setInterval(() => {
+					num += 1;
+					if (connected) {
+						clearInterval(intevalId);
+						messageQueue.add({type: "reload"});
+						spinner.stop();
+					} else if (num === count) {
+						clearInterval(intevalId);
+						opn(`http://localhost:${port}`);
+						spinner.stop();
+					}
+				}, 1000);
+			});
 		});
 	}
 };
