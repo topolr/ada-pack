bdb04e8957a1f0769d6d718a6ec22b5727468312
Jinliang <hou80houzhu@vip.qq.com>
2018-11-15 21:14:13 +0800

    pass

--- src/lib/maker/htmlmaker.js
+++ src/lib/maker/htmlmaker.js
@@ -1,17 +1,7 @@
-let minify = require('html-minifier').minify;
-let util = require("../../util/helper");
-let Path = require("path");
-
 module.exports = function ({content, path, option}) {
     return new Promise((resolve, reject) => {
         try {
             content = content.replace(/>[\s]+</g, "><").replace(/\r\n/g, "").replace(/\r/g, "").replace(/\n/g, "");
-            // resolve(minify(content, Object.assign({
-            //     removeComments: true,
-            //     collapseWhitespace: true,
-            //     minifyJS: true,
-            //     minifyCSS: true
-            // }, option.minifier)));
             resolve(content);
         } catch (e) {
             reject(e);
